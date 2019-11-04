4a338d192f6b37dbdf76d9ed5dbb43a607ef127a
frahmadani <frahmadani@gmail.com>
2019-11-04 15:20:14 +0700

    initial commit

---
+++ client.js
@@ -0,0 +1,58 @@
+// This file contains the boilerplate to execute your React app.
+// If you want to modify your application's content, start in "index.js"
+
+import { ReactInstance, Surface } from 'react-360-web';
+
+function init(bundle, parent, options = {}) {
+  const r360 = new ReactInstance(bundle, parent, {
+    // Add custom options here
+    fullScreen: true,
+    ...options,
+  });
+
+  // Create separate panels for buttons and info
+  const buttonsPanel = new Surface(
+    400,
+    550,
+    Surface.SurfaceShape.Flat
+  )
+
+  buttonsPanel.setAngle(
+    -0.6,
+    0.1
+  )
+
+  const infoPanel = new Surface(
+    400,
+    550,
+    Surface.SurfaceShape.Flat
+  )
+
+  infoPanel.setAngle(
+    0.6,
+    0.1
+  )
+
+  // Render your app content to the default cylinder surface
+  r360.renderToSurface(
+    r360.createRoot('Buttons', { /* initial props */ }),
+
+    // Use custom surfaces for button and info
+    // Comment below line to use the default surface instead
+    buttonsPanel
+
+    // Uncomment below to use the default surface instead
+    // r360.getDefaultSurface()
+  );
+
+  // Render the second surface (infoPanel)
+  r360.renderToSurface(
+    r360.createRoot('InfoPanel', { /* initial props */ }),
+    infoPanel
+  );
+
+  // Load the initial environment
+  r360.compositor.setBackground(r360.getAssetURL('360_front_door.jpg'));
+}
+
+window.React360 = { init };
---
+++ index.js
@@ -0,0 +1,140 @@
+import React from 'react';
+import {
+  AppRegistry,
+  asset,
+  Environment,
+  StyleSheet,
+  Text,
+  View,
+  VrButton
+} from 'react-360';
+import house from './data/houseData';
+
+export default class Buttons extends React.Component {
+  // state always hold info about current room
+  state = {
+    room: house.House.roomName,
+    info: house.House.info,
+    adjacentRooms: house.House.adjacentRooms
+  }
+
+  // handle click to change state
+  clickHandler(roomSelection) {
+    this.setState({
+      room: house[`${roomSelection}`].roomName,
+      info: house[`${roomSelection}`].info,
+      adjacentRooms: house[`${roomSelection}`].adjacentRooms
+    })
+
+    Environment.setBackgroundImage(asset(`./360_${house[`${roomSelection}`].img}`))
+  }
+
+  createRoomButtons(adjacentRooms) {
+    let rooms = adjacentRooms;
+    let buttons = [];
+
+    // spawn the buttons to change rooms, complete with its clickHandlers
+    rooms.map(room => {
+      buttons.push(
+        <VrButton key={`${room}` + `-button`} onClick={() => this.clickHandler(room)}>
+          <Text style={{ backgroundColor: 'green' }}> {room}</Text>
+        </VrButton>
+      )
+    })
+
+    return buttons;
+
+  }
+
+  render() {
+    return (
+      <View style={styles.panel}>
+        <View style={styles.greetingBox}>
+          <Text>Room Selection</Text>
+          <Text> {this.state.room} </Text>
+          {this.createRoomButtons(this.state.adjacentRooms)}
+        </View>
+
+        <View style={styles.greetingBox}>
+          <Text>Room Info</Text>
+          <Text> {this.state.info} </Text>
+        </View>
+      </View >
+    );
+  }
+};
+
+export class InfoPanel extends React.Component {
+  // state always hold info about current room
+  state = {
+    room: house.House.roomName,
+    info: house.House.info,
+    adjacentRooms: house.House.adjacentRooms
+  }
+
+  // handle click to change state
+  clickHandler(roomSelection) {
+    this.setState({
+      room: house[`${roomSelection}`].roomName,
+      info: house[`${roomSelection}`].info,
+      adjacentRooms: house[`${roomSelection}`].adjacentRooms
+    })
+
+    Environment.setBackgroundImage(asset(`./360_${house[`${roomSelection}`].img}`))
+  }
+
+  createRoomButtons(adjacentRooms) {
+    let rooms = adjacentRooms;
+    let buttons = [];
+
+    // spawn the buttons to change rooms, complete with its clickHandlers
+    rooms.map(room => {
+      buttons.push(
+        <VrButton key={`${room}` + `-button`} onClick={() => this.clickHandler(room)}>
+          <Text style={{ backgroundColor: 'green' }}> {room}</Text>
+        </VrButton>
+      )
+    })
+
+    return buttons;
+
+  }
+
+  render() {
+    return (
+      <View style={styles.panel}>
+        <View style={styles.greetingBox}>
+          <Text>Room Selection</Text>
+          <Text> {this.state.room} </Text>
+          {this.createRoomButtons(this.state.adjacentRooms)}
+        </View>
+
+        <View style={styles.greetingBox}>
+          <Text>Room Info</Text>
+          <Text> {this.state.info} </Text>
+        </View>
+      </View >
+    );
+  }
+};
+
+const styles = StyleSheet.create({
+  panel: {
+    // Fill the entire surface
+    width: 1000,
+    height: 600,
+    backgroundColor: 'rgba(255, 255, 255, 0.4)',
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  greetingBox: {
+    padding: 20,
+    backgroundColor: '#000000',
+    borderColor: '#639dda',
+    borderWidth: 2,
+  }
+});
+
+AppRegistry.registerComponent('Buttons', () => Buttons);
+AppRegistry.registerComponent('InfoPanel', () => InfoPanel);
+
